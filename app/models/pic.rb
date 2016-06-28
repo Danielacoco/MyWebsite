1dc1618314f73e5eb8cca8d61e503bf0fd263401
Danielacoco <danielacoor6@gmail.com>
2016-06-28 12:38:02 -0700

    added usernames to users andshowed them under pics

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -5,4 +5,6 @@ class Pic < ActiveRecord::Base
 										:styles => { :medium => "700x700", :thumb => "500X500"}
 
 	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+	validates :image, presence: true
+	validates :description, presence: true
 end
